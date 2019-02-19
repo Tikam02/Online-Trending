81f9a9d21d54cdfd6345de78094d720c2fabd64f
[Tikam02] <timonalma81@gmail.com>
2019-02-20 00:40:45 +0530

    script

--- apex/apex.sh
+++ apex/apex.sh
@@ -1,8 +1,39 @@
 #!/bin/bash
 
+color() {
+     echo "$@" | sed \
+             -e "s/\(\(@\(red\|green\|yellow\|blue\|magenta\|cyan\|white\|reset\|b\|u\)\)\+\)[[]\{2\}\(.*\)[]]\{2\}/\1\4@reset/g" \
+             -e "s/@red/$(tput setaf 1)/g" \
+             -e "s/@green/$(tput setaf 2)/g" \
+             -e "s/@yellow/$(tput setaf 3)/g" \
+             -e "s/@blue/$(tput setaf 4)/g" \
+             -e "s/@magenta/$(tput setaf 5)/g" \
+             -e "s/@cyan/$(tput setaf 6)/g" \
+             -e "s/@white/$(tput setaf 7)/g" \
+             -e "s/@reset/$(tput sgr0)/g" \
+             -e "s/@b/$(tput bold)/g" \
+             -e "s/@u/$(tput sgr 0 1)/g"
+  }
+  
+color @b@blue
+
+echo " █████╗ ██████╗ ███████╗██╗  ██╗"
+echo "██╔══██╗██╔══██╗██╔════╝╚██╗██╔╝"
+echo "███████║██████╔╝█████╗   ╚███╔╝ "
+echo "██╔══██║██╔═══╝ ██╔══╝   ██╔██╗ "
+echo "██║  ██║██║     ███████╗██╔╝ ██╗"
+echo "╚═╝  ╚═╝╚═╝     ╚══════╝╚═╝  ╚═╝"
+echo "-------------------------------------------------"
+echo " Online-Trending WebApp - SEN Project - Group 2 "
+echo "--------------------------------------------------"
+
+
+
 function ask_user() {    
 
-echo -e "Default \e[93mLight yellow"
+
+
+color @b@cyan
 
 echo -e "
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
@@ -46,7 +77,7 @@ elif [ "$choice" == "5" ]; then
 
 function ask_for_services() {
 
-echo -e "Default \e[96mLight cyan"
+color @b@yellow
 
 echo -e "
 #-----------------------------------#
