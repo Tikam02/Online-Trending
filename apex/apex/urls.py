acfbcdeee8efc5eff15794a1e5b2fa5ae8d7071b
Nidhi <nidhi98gupta@gmail.com>
2019-04-04 15:14:25 +0530

    Added Forgot Password

--- apex/apex/urls.py
+++ apex/apex/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.contrib.auth import views as auth_views
+from django.contrib.auth import urls as url_views
 from django.urls import include, path
 from django.views.generic.edit import CreateView
 from apex.apps.core import views as core_views
@@ -46,6 +47,14 @@ urlpatterns = [
     path('<slug:slug>/', include('apex.apps.services.urls', namespace='services')),
     path('<pk>/bookmark/',services_views.BookmarkView.as_view(model=BookmarkArticle),
         name='article_bookmark'),
+    # url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
+    # url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    # url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+    #     auth_views.password_reset_confirm, name='password_reset_confirm'),
+    # url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
+
+    path('accounts/', include('django.contrib.auth.urls')),
+    #path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     # url('bbc-sport/article/(?P<pk>\d+)/bookmark/',services_views.BookmarkView.as_view(model=BookmarkArticle),
     #     name='article_bookmark'),
 ]
