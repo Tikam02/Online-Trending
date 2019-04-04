f2aa6a6f44c381c58b6c333326917db0b3fbab13
Nidhi <nidhi98gupta@gmail.com>
2019-04-04 23:00:03 +0530

    Fix login bug

--- apex/apex/urls.py
+++ apex/apex/urls.py
@@ -17,9 +17,7 @@ Including another URLconf
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.contrib.auth import views as auth_views
-from django.contrib.auth import urls as url_views
 from django.urls import include, path
-from django.views.generic.edit import CreateView
 from apex.apps.core import views as core_views
 from apex.apps.services import views as services_views
 from apex.templates.services.includes import views as B_view
@@ -53,7 +51,14 @@ urlpatterns = [
     #     auth_views.password_reset_confirm, name='password_reset_confirm'),
     # url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
 
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('accounts/password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+
+    path('accounts/password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('accounts/password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('accounts/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('accounts/reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
     #path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     # url('bbc-sport/article/(?P<pk>\d+)/bookmark/',services_views.BookmarkView.as_view(model=BookmarkArticle),
     #     name='article_bookmark'),
