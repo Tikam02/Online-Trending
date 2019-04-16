1ae86309aa13e60eefb383884e1f5c3b4b41a246
Nidhi <nidhi98gupta@gmail.com>
2019-04-16 16:03:36 +0530

    Copy db for testing

--- apex/apex/testing/test_bookmarks.py
+++ apex/apex/testing/test_bookmarks.py
@@ -1,12 +1,14 @@
 from django.contrib.auth.models import User
 from django.test import TestCase,Client
 from apex.apps.services.models import *
+import pytest
 
 class TestBookmarks(TestCase):
 
 	#add Bookmark
+	@pytest.mark.django_db
 	def test_1(self):
-		user1 = User.objects.all().count()
+		user1 = Story.objects.all().count()
 		# story = Story.objects.filter(id=4)
 		# self.client.post('47/bookmark/',{'user': "user1"})
 		assert user1 > 0
