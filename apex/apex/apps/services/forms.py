457880866285a3a0586c309318d811cce13f21c9
Swapnil Dhimmar <sdhimmar006@gmail.com>
2019-04-17 13:16:04 +0530

    Added categories

--- apex/apex/apps/services/forms.py
+++ apex/apex/apps/services/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-
 class RegistrationForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
